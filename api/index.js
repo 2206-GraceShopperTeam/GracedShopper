f77c0c84c6ec37ba99a30e3147cb6715c9295354
Thomas <thomas-g-smith@hotmail.com>
2022-08-10 18:06:27 -0500

    Built api functions for /login and /register tested with curl

--- api/index.js
+++ api/index.js
@@ -1,17 +1,65 @@
-const apiRouter = require('express').Router();
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
+const { getUserById } = require("../db");
 
-apiRouter.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
   res.send({
     message: 'API is under construction!',
   });
 });
 
-apiRouter.get('/health', (req, res, next) => {
+router.get('/health', (req, res, next) => {
   res.send({
     healthy: true,
   });
 });
 
+router.use(async (req, res, next) => {
+  const prefix = "Bearer ";
+  const auth = req.header("Authorization");
+
+  if (!auth) {
+    // nothing to see here
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  } else {
+    next({
+      name: "AuthorizationHeaderError",
+      message: `Authorization token must start with ${prefix}`,
+    });
+  }
+});
+
 // place your routers here
 
-module.exports = apiRouter;
+// ROUTER: /api/users
+const usersRouter = require("./users");
+router.use("/users", usersRouter);
+
+// // ROUTER: /api/activities
+// const activitiesRouter = require("./products");
+// router.use("/activities", activitiesRouter);
+
+// // ROUTER: /api/cart
+// const cartRouter = require("./cart");
+// router.use("/cart", cartRouter);
+
+// // ROUTER: /api/checkout
+// const checkoutRouter = require("./checkout");
+// router.use("/checkout", checkoutRouter);
+
+module.exports = router;
--- db/index.js
+++ db/index.js
@@ -2,4 +2,5 @@ const client = require('./client');
 
 module.exports = {
   client,
+  ...require("./users")
 };
