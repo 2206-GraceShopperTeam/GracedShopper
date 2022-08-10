d982616b6beef70f058e20f43051e2696bc4168a
John-Anthony <jharwerth@udallas.edu>
2022-08-10 14:08:29 -0500

    progress

--- db/cart.js
+++ db/cart.js
@@ -1,19 +1,95 @@
 const client = require("./client");
 
-async function createCart() {}
+async function attachProductsToCarts() {}
 
-async function getCartById() {}
+async function createCarts({/* help */}) {
+    try {
+        const {
+          rows: [cart],
+        } = await client.query(
+          `
+          INSERT INTO carts(/* help */) 
+          VALUES(/* help */)
+          ON CONFLICT (name) DO NOTHING
+          RETURNING *;
+        `,
+          [/* help */]
+        );
+    
+        return cart;
+      } catch (error) {
+        throw error;
+      }
+}
 
-async function getCartWithoutCartProducts() {}
+async function getAllCarts() {}
 
-async function getCart() {}
+async function getCartsById() {}
 
-async function getCartByUser() {}
+async function getCartsByUser() {}
 
+async function getCartsWithoutProducts() {
+    try {
+        const { rows } = await client.query(
+          `
+        SELECT *
+        FROM carts;
+      `
+        );
+        if (!rows) {
+          return null;
+        }
+        return rows;
+      } catch (error) {
+        throw error;
+      }
+}
 
+async function updateCarts({ id, ...fields }) {
+    const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
 
-async function attachCartProductsToCart() {}
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const {
+      rows: [cart],
+    } = await client.query(
+      `
+      UPDATE carts
+      SET ${setString}
+      WHERE id=${id}
+      RETURNING *;
+    `,
+      Object.values(fields)
+    );
+
+    return cart;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function destroyCarts(id) {
+    await client.query(
+        `
+        DELETE FROM carts
+        WHERE id=$1
+      `,
+        [id]
+      );
+}
 
 module.exports = {
-  attachCartProductsToCart,
+  attachProductsToCarts,
+  createCarts,
+  getCartsById,
+  getCartsWithoutProducts,
+  getAllCarts,
+  getCartsByUser,
+  updateCarts,
+  destroyCarts,
 };
