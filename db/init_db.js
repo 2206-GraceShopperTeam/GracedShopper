41f54adbf875ddbf8004a035d29ad77976fe4e9f
Thomas <thomas-g-smith@hotmail.com>
2022-08-10 08:52:28 -0500

    started working on building tables

--- db/init_db.js
+++ db/init_db.js
@@ -4,29 +4,75 @@ const {
   // for example, User
 } = require('./');
 
-async function buildTables() {
+const client = require("./client");
+
+async function dropTables() {
   try {
-    client.connect();
-
-    // drop tables in correct order
-
-    // build tables in correct order
+    console.log("Dropping All Tables...");
+    // drop all tables, in the correct order
+    await client.query(`
+  DROP TABLE IF EXISTS "routineActivities";
+  DROP TABLE IF EXISTS routine_activities;
+  DROP TABLE IF EXISTS routines;
+  DROP TABLE IF EXISTS activities;
+  DROP TABLE IF EXISTS users;
+  `);
   } catch (error) {
     throw error;
   }
 }
 
-async function populateInitialData() {
+
+
+async function createTables() {
+  console.log("Starting to build tables...");
   try {
-    // create useful starting data by leveraging your
-    // Model.method() adapters to seed your db, for example:
-    // const user1 = await User.createUser({ ...user info goes here... })
+    await client.query(
+      `CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+              email VARCHAR(255) UNIQUE NOT NULL,
+              password VARCHAR(255) NOT NULL,
+              "firstName" VARCHAR(255) NOT NULL,
+              address TEXT,
+      
+      
+              `);
   } catch (error) {
     throw error;
   }
 }
 
-buildTables()
-  .then(populateInitialData)
-  .catch(console.error)
-  .finally(() => client.end());
+async function createInitialUsers() {
+  console.log("Starting to create users...");
+  try {
+    const usersToCreate = [
+      { email: "graces@hopper.com", password: "momofall", firstName: grace },
+      { email: "hoppers@hopper.com", password: "trappedinrussia",firstName: hopper },
+      { email: "eleven@ontherun.com", password: "hasallthepower",firstName: eleven },
+    ];
+    const users = await Promise.all(usersToCreate.map(createUser));
+
+    console.log("Users created:");
+    console.log(users);
+    console.log("Finished creating users!");
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function rebuildDB() {
+  try {
+    await dropTables();
+    await createTables();
+  } catch (error) {
+    console.log("Error during rebuildDB");
+    throw error;
+  }
+}
+
+
+module.exports = {
+  rebuildDB,
+  dropTables,
+  createTables,
+};
