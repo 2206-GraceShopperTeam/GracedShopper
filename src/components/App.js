1cebcce723515441853dafc0389d8f32ef82c221
giohicar <giohicar@gmail.com>
2022-08-17 14:18:16 -0500

    some changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import { getAPIHealth } from '../axios-services';
-import {Home,Header,Login,Products,Cart, Checkout} from './'
-import '../style/App.css';
+import { getAPIHealth } from "../axios-services";
+import { Home, Header, Login, Products, Cart, Checkout } from "./";
+import "../style/App.css";
 
 const App = () => {
-const [loggedIn,setLoggedIn] = useState(false)
-const [user,setUser] = useState({})
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
 
-  useEffect(() => {
-    
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <div className="app-container">
-      <Header setLoggedIn={setLoggedIn} loggedIn={loggedIn}/>
+      <Header setLoggedIn={setLoggedIn} loggedIn={loggedIn} />
       <Routes>
-
-         <Route path="/Login" element={<Login setLoggedIn={setLoggedIn} loggedIn={loggedIn} user={user} setUser={setUser}/>} />
-         <Route path="/Products" element={<Products/>} />
-         <Route path="/Cart" element={<Cart/>} />
-         <Route path="/" element={<Home/>} />
-         
+        <Route path="/Checkout" element={<Checkout />} />
+        <Route
+          path="/Login"
+          element={
+            <Login
+              setLoggedIn={setLoggedIn}
+              loggedIn={loggedIn}
+              user={user}
+              setUser={setUser}
+            />
+          }
+        />
+        <Route path="/Products" element={<Products />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,13 +1,24 @@
 import React, { useEffect, useState } from "react";
 import {  useNavigate } from "react-router-dom";
+import { getCartProducts } from './'
 
 const Checkout = (props) => {
 
-    
+    const [address, setAddress] = useState('')
+    const [email, setEmail] = useState('')
+    const [name, setName] = useState('')
+    const [productName, setProductName] = useState('')
+    const [price, setPrice] = useState('')
 
+    const cartProducts = await getCartProducts()
     return(
-        <div>
-            <h1>This is checkout</h1>
+        <div className="cart">
+            <h1>Order Details</h1>
+            <div>
+                <div className="checkoutproducts">
+
+                </div>
+            </div>
         </div>
     )
 }
