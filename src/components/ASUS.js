237796ebfc453ad526b81c85bd9192b4e2ab64ce
John-Anthony <jharwerth@udallas.edu>
2022-08-18 09:33:48 -0500

    clean up

--- src/components/ASUS.js
+++ src/components/ASUS.js
@@ -1,40 +1,64 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { getProducts } from "../axios-services";
+import { useNavigate } from "react-router";
 
 const ASUS = () => {
-    const [allProducts, setAllProducts] = useState([]);
-  const token = localStorage.getItem("token")
+  let navigate = useNavigate();
+  const [allProducts, setAllProducts] = useState([]);
 
   useEffect(() => {
     async function fetchProducts() {
       const returnProducts = await getProducts();
       setAllProducts(returnProducts);
-      
     }
     fetchProducts();
   }, []);
-  function randomCents(){
-     let change = Math.random() * 100;
-     change = Math.floor(change);
-     return change;
-   }
+
+  const dellHandleClick = (event) => {
+    event.preventDefault();
+    navigate("/Products/Dell");
+  };
+  const hpHandleClick = (event) => {
+    event.preventDefault();
+    navigate("/Products/HP");
+  };
+  const asusHandleClick = (event) => {
+    event.preventDefault();
+    navigate("/Products/ASUS");
+  };
+  const appleHandleClick = (event) => {
+    event.preventDefault();
+    navigate("/Products/Apple");
+  };
+
+  function randomCents() {
+    let change = Math.random() * 100;
+    change = Math.floor(change);
+    return change;
+  }
+
   return (
     <div className="products">
+      <h1>ASUS</h1>
+      <button onClick={dellHandleClick}>Dell</button>
+      <button onClick={hpHandleClick}>HP</button>
+      <button onClick={asusHandleClick}>ASUS</button>
+      <button onClick={appleHandleClick}>Apple</button>
       {allProducts.length
         ? allProducts.map((product) => {
-          return (product.category === "Asus") ? 
-          <div
-                className="greenBox"
-                key={`Products${product.id}`}
-              >
+            return product.category === "Asus" ? (
+              <div className="greenBox" key={`Products${product.id}`}>
                 <p>Name: {product.name}</p>
                 <p>Description: {product.description}</p>
-                <p>Price: ${product.price}.{}</p>
+                <p>
+                  Price: ${product.price}..{randomCents()}
+                </p>
                 <p>Brand: {product.category}</p>
                 <button>Add to Cart</button>
               </div>
-        : null})
-        :null}
+            ) : null;
+          })
+        : null}
     </div>
   );
 };
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,73 +1,58 @@
-import React, {useState,useEffect} from "react";
-import { getProducts,addToCart } from "../axios-services";
+import React, { useState, useEffect } from "react";
+import { getProducts, addToCartProducts } from "../axios-services";
 import { useNavigate } from "react-router";
-
-
 
 const Products = () => {
   let navigate = useNavigate();
-
   const [allProducts, setAllProducts] = useState([]);
-  const token = localStorage.getItem("token")
 
   useEffect(() => {
     async function fetchProducts() {
       const returnProducts = await getProducts();
       setAllProducts(returnProducts);
-      
     }
     fetchProducts();
   }, []);
 
   const dellHandleClick = (event) => {
-    event.preventDefault()
-    navigate("/Products/Dell")
-  }
+    event.preventDefault();
+    navigate("/Products/Dell");
+  };
   const hpHandleClick = (event) => {
-    event.preventDefault()
-    navigate("/Products/HP")
-  }
+    event.preventDefault();
+    navigate("/Products/HP");
+  };
   const asusHandleClick = (event) => {
-    event.preventDefault()
-    navigate("/Products/ASUS")
-  }
+    event.preventDefault();
+    navigate("/Products/ASUS");
+  };
   const appleHandleClick = (event) => {
-    event.preventDefault()
-    navigate("/Products/Apple")
+    event.preventDefault();
+    navigate("/Products/Apple");
+  };
+
+  function randomCents() {
+    let change = Math.random() * 100;
+    change = Math.floor(change);
+    return change;
   }
-
-
-
-  function randomCents(){
-     let change = Math.random() * 100;
-     change = Math.floor(change);
-     return change;
-   }
 
   return (
     <div className="products">
-      <button onClick={dellHandleClick}>
-        Dell
-      </button>
-      <button onClick={hpHandleClick}>
-        HP
-      </button>
-      <button onClick={asusHandleClick}>
-        ASUS
-      </button>
-      <button onClick={appleHandleClick}>
-        Apple
-      </button>
+      <h1>All Laptops</h1>
+      <button onClick={dellHandleClick}>Dell</button>
+      <button onClick={hpHandleClick}>HP</button>
+      <button onClick={asusHandleClick}>ASUS</button>
+      <button onClick={appleHandleClick}>Apple</button>
       {allProducts.length
         ? allProducts.map((product) => {
             return (
-              <div
-                className="greenBox"
-                key={`Products${product.id}`}
-              >
+              <div className="greenBox" key={`Products${product.id}`}>
                 <p>Name: {product.name}</p>
                 <p>Description: {product.description}</p>
-                <p>Price: ${product.price}.{randomCents()}</p>
+                <p>
+                  Price: ${product.price}.{randomCents()}
+                </p>
                 <p>Brand: {product.category}</p>
                 <button>Add to cart</button>
               </div>
