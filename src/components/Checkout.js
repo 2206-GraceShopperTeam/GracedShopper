367f003cd6c2375306a38c70d7b329e2d99eb8c7
giohicar <giohicar@gmail.com>
2022-08-18 14:39:56 -0500

    finished checkout

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -10,13 +10,12 @@ const Checkout = () => {
   const [email, setEmail] = useState("");
   const [productName, setProductName] = useState("");
   const [check, setCheck] = useState(false);
+  const [purchased, setPurchased] = useState(false)
   var myCart = [];
 
-  console.log(productName, " cartproducts");
   useEffect(() => {
     async function fetchCartProducts() {
       const returnCartProducts = await getCartProducts();
-      console.log(returnCartProducts, "this is cartProducts");
       setCartProducts(returnCartProducts);
       //   for (var x = 0; x < returnCartProducts.length; x++) {
       //     if (returnCartProducts[x].cart_id === 1) {
@@ -25,22 +24,20 @@ const Checkout = () => {
       //     }
       //   }
     }
-    console.log(myCart.length);
     if (!myCart.length > 0) {
       setCheck(true);
     }
     fetchCartProducts();
   }, [check]);
-  console.log(cartProducts, "this is myCaart");
   return (
     <div className="checkout">
-      {cartProducts.map((product) => {
+        <div className={purchased ? "hidden" : 'orderInfo'}>
+      {cartProducts.map((product, index) => {
         return product.cart_id === 1 ? (
-          <div>
-            <hello>hello</hello>
+          <div key={index} className='cartProduct'>
             <h3>{product.name}</h3>
-            <p>{product.quantity}</p>
-            <p>{product.price}</p>
+            <p>({product.quantity})</p>
+            <h3>${product.price}</h3>
           </div>
         ) : null;
       })}
@@ -48,19 +45,11 @@ const Checkout = () => {
         <h3>{email}</h3>
         <h3>{address}</h3>
       </form>
+      <button onClick={()=>{setPurchased(!purchased)}}>Purchase</button>
+      </div>
+      <div className={!purchased ? 'hidden' : 'purchaseMessage'}>Thank You For Your Money!</div>
     </div>
   );
 };
 
 export default Checkout;
-// {product.id === 1 ? return (
-//               <div>
-//                 <hello>hello</hello>
-//                 <h3>{product.name}</h3>
-//                 <p>{product.quantity}</p>
-//                 <p>{product.price}</p>
-//               </div>
-//             ) : null}
-//             console.log(product, 'this is product')
-//             ;:null
-//           })
