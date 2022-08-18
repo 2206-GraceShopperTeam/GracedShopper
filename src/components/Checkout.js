5b9e55f23b8cd6ad0e7dfc3431f19fbedc96cac4
Thomas <thomas-g-smith@hotmail.com>
2022-08-18 13:49:43 -0500

    worked on mapping products by cart

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -4,51 +4,47 @@ import { getCartProducts } from "../axios-services";
 import "../style/Checkout.css";
 
 const Checkout = () => {
-  const [cartProducts, setCartProducts] = useState("");
+  const [cartProducts, setCartProducts] = useState([]);
 
   const [address, setAddress] = useState("");
   const [email, setEmail] = useState("");
   const [productName, setProductName] = useState("");
-  const [check, setCheck] = useState(false)
+  const [check, setCheck] = useState(false);
   var myCart = [];
 
   console.log(productName, " cartproducts");
   useEffect(() => {
     async function fetchCartProducts() {
       const returnCartProducts = await getCartProducts();
-      console.log(returnCartProducts, 'this is cartProducts')
-      for (var x = 0; x < returnCartProducts.length; x++) {
-        if (returnCartProducts[x].cart_id === 1) {
-          myCart.push(returnCartProducts[x]);
-          console.log(myCart, 'this is myCart')
-        }
-      }
+      console.log(returnCartProducts, "this is cartProducts");
+      setCartProducts(returnCartProducts);
+      //   for (var x = 0; x < returnCartProducts.length; x++) {
+      //     if (returnCartProducts[x].cart_id === 1) {
+      //       myCart.push(cartProducts[x]);
+      //       console.log(myCart, 'this is myCart')
+      //     }
+      //   }
     }
-    console.log(myCart.length)
-    if(!myCart.length > 0){
-        setCheck(true)
+    console.log(myCart.length);
+    if (!myCart.length > 0) {
+      setCheck(true);
     }
     fetchCartProducts();
   }, [check]);
-  console.log(myCart, 'this is myCaart')
+  console.log(cartProducts, "this is myCaart");
   return (
     <div className="checkout">
+      {cartProducts.map((product) => {
+        return product.cart_id === 1 ? (
+          <div>
+            <hello>hello</hello>
+            <h3>{product.name}</h3>
+            <p>{product.quantity}</p>
+            <p>{product.price}</p>
+          </div>
+        ) : null;
+      })}
       <form>
-          { !check ? (
-            <div>No items</div>
-          ) :
-          myCart.map((product, index) => {
-            console.log(product, 'this is product')
-            return (
-              <div>
-                <hello>hello</hello>
-                <h3>{product.name}</h3>
-                <p>{product.quantity}</p>
-                <p>{product.price}</p>
-              </div>
-            );
-          })
-        }
         <h3>{email}</h3>
         <h3>{address}</h3>
       </form>
@@ -57,3 +53,14 @@ const Checkout = () => {
 };
 
 export default Checkout;
+// {product.id === 1 ? return (
+//               <div>
+//                 <hello>hello</hello>
+//                 <h3>{product.name}</h3>
+//                 <p>{product.quantity}</p>
+//                 <p>{product.price}</p>
+//               </div>
+//             ) : null}
+//             console.log(product, 'this is product')
+//             ;:null
+//           })
